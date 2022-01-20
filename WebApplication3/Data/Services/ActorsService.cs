669250bae10c1e6b5fdee4195c5632ac178931a2
Lushen Moodley <lushenmoodley@yahoo.com>
2022-01-20 08:04:40 +0200

    Producer feature created

---
+++ WebApplication3/Data/Services/ActorsService.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication3.Models;
+
+namespace WebApplication3.Data.Services
+{
+    public class ActorsService : IActorsServices
+    {
+        private readonly AppDbContext _context;
+
+        public ActorsService(AppDbContext context)
+        {
+            _context = context;
+
+        }
+
+        public void Add(Actor actor)
+        {
+            _context.Actors.Add(actor);
+            _context.SaveChanges();
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IEnumerable<Actor>> GetAll()
+        {
+            var results = await _context.Actors.ToListAsync();
+            return results;
+        }
+
+        public Actor GetById(int id)
+        {
+            throw new NotImplementedException();
+
+        }
+
+        public Actor Update(int id, Actor newActor)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ WebApplication3/Data/Services/IActorsServices.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication3.Models;
+
+namespace WebApplication3.Data.Services
+{
+    public interface IActorsServices //An interface is a contract 
+    {
+        Task<IEnumerable<Actor>> GetAll();//returning a task Ienumerable
+        Actor GetById(int id);
+        void Add(Actor actor);
+        Actor Update(int id,Actor newActor);
+        void Delete(int id);
+    }
+}
